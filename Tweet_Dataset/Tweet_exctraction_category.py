39124b02f560092e050f13f3532e73fc238bdd88
Praver Vats <pv335@snu.edu.in>
2017-08-21 10:15:11 +0530

    Update Tweet_exctraction_category.py

--- Tweet_Dataset/Tweet_exctraction_category.py
+++ Tweet_Dataset/Tweet_exctraction_category.py
@@ -2,28 +2,26 @@
 
 import tweepy
 from tweepy import OAuthHandler
-import textblob
-import re
 import json
 import time
-import codecs
-import csv
 
-*
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-word_list = ['#booz','#wasted','#alcohol','#beer','#whiskey']
+word_list = ['booz','wasted','alcohol','beer','whiskey']
 
-for a in word_list:
+def get_tweets_category(word_list):
+    for a in word_list:
+        Tweets = []
         tweets = api.search(q = a, count = 100)
         for tweet in tweets:
-            #tweet.text = tweet.text.replace('RT',' ')
-            tweet.text = tweet.text.replace('\n','\r\n')
             tweet.text = tweet.text.replace('&amp;', '&')
-            f.write(str(tweet.text))
-            f.write(",Alcohol")
-            f.write("\n\n")
+            Tweets.append(tweet.text)
         time.sleep(60)
+        return Tweets
