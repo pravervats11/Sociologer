3821d22f45095ae1a06b1ca5f595d3691c4870e1
Praver Vats <pv335@snu.edu.in>
2017-09-09 09:21:22 +0530

    Update Text_preprocessing.py

--- Text_preprocessing.py
+++ Text_preprocessing.py
@@ -5,6 +5,6 @@ import nltk
 
 '''
 Text preprocessing for feeding into text classifier for it to learn.
-This algo is different from that in Tweet_preprocessing.py preprocessing.
+This algorithm is different from that in Tweet_preprocessing.py preprocessing.
 '''
 
